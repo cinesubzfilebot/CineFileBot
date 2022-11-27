f9142ae845e2b47c178eb3d75540f2ef2007652c
cineleechpro1 <109298114+cineleechpro1@users.noreply.github.com>
2022-11-27 21:05:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,10 +101,8 @@ async def start(bot: Client, cmd: Message):
                 )
             else:
                 message_ids.append(int(GetMessage.id))
-            for i in range(len(message_ids)):
-                await send_media_and_reply(bot, user_id=cmd.from_user.id, file_id=int(message_ids[i]))
-        except Exception as err:
-            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
+
+
 
 
 @Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.chat(Config.DB_CHANNEL))
