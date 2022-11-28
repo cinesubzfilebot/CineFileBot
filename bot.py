a66a274ebe35d8b7f0104a1b6f0d344d841675a2
cineleechpro1 <109298114+cineleechpro1@users.noreply.github.com>
2022-11-28 22:33:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,28 +84,6 @@ async def start(bot: Client, cmd: Message):
                 ]
             )
         )
-    else:
-        try:
-            try:
-                file_id = int(b64_to_str(usr_cmd).split("_")[-1])
-            except (Error, UnicodeDecodeError):
-                file_id = int(usr_cmd.split("_")[-1])
-            GetMessage = await bot.get_messages(chat_id=Config.DB_CHANNEL, message_ids=file_id)
-            message_ids = []
-            if GetMessage.text:
-                message_ids = GetMessage.text.split(" ")
-                _response_msg = await cmd.reply_text(
-                    text=f"**Total Files:** `{len(message_ids)}`",
-                    quote=True,
-                    disable_web_page_preview=True
-                )
-            else:
-                message_ids.append(int(GetMessage.id))
-            for i in range(len(message_ids)):
-                await cmd.reply_text(f"")
-        except Exception as err:
-            await cmd.reply_text(f"")
-
 
 @Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.chat(Config.DB_CHANNEL))
 async def main(bot: Client, message: Message):
