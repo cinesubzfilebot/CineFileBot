b3bc6762f7fdcb6fdb873a288f9e7bec1723011d
cineleechpro1 <109298114+cineleechpro1@users.noreply.github.com>
2022-11-28 22:26:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,9 +128,13 @@ async def main(bot: Client, message: Message):
             return
 
         await message.reply_text(
-            text="",
-            quote=False,
-            disable_web_page_preview=False
+            text="**Please Join Here 👇**",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("🏆 Channel 🏆", url="https://t.me/CineSubzMovies")],
+                [InlineKeyboardButton("🗣 Group 🗣 ", url="https://t.me/CineSubzChatNew")]
+            ]),
+            quote=True,
+            disable_web_page_preview=True
         )
     elif message.chat.type == enums.ChatType.CHANNEL:
         if (message.chat.id == int(Config.LOG_CHANNEL)) or (message.chat.id == int(Config.UPDATES_CHANNEL)) or message.forward_from_chat or message.forward_from:
